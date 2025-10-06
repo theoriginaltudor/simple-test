194d4ad08406a5c73fdb5912cbe3c2b0eeb59c8e
Tudor Caseru <theoriginaltudor@gmail.com>
2025-10-06 13:37:08 +0200

    clean css and add tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-export const pluginList = [react()];
+export const pluginList = [react(), tailwindcss()];
 
 // https://vite.dev/config/
 export default defineConfig({
