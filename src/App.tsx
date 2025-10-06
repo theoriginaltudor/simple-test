d8b3536638f8eaf5e25bf754c0616f8dbe8f43fb
Tudor Caseru <theoriginaltudor@gmail.com>
2025-10-06 15:45:19 +0200

    extend text

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
 import { useState } from "react";
+import { Text } from "./components/text";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1 className="text-3xl text-green-300">Vite + React</h1>
-      <div className="card">
+      <Text preset={1} as={"h1"} className="text-grey-400">
+        Vite + React
+      </Text>
+
+      <div className="rounded-2xl bg-orange-400 m-1 p-3">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
+        <p className="text-green-400 text-sm leading-4 tracking-normal">
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="text">Click on the Vite and React logos to learn more</p>
+      <p className="text-green-200 font-space-mono">
+        <Text>Click on the Vite and React logos to learn more</Text>
+      </p>
     </>
   );
 }
--- src/components/text.tsx
+++ src/components/text.tsx
@@ -1,24 +1,31 @@
-import type { ReactNode } from "react";
+import type { ElementType, ReactNode } from "react";
 import { cn } from "../../utils";
 
 interface Props {
   preset?: 1 | 2 | 3 | 4 | 5 | 6;
   children: ReactNode;
+  as?: ElementType;
+  className?: string;
 }
 
-export const Text = ({ preset = 6, children }: Props) => {
+export const Text = ({
+  preset = 6,
+  children,
+  as: El = "span",
+  className,
+}: Props) => {
   const presetList = [
     "text-5xl leading-[71px] -tracking-[1px]",
     "text-[42px] leading-[47px] -tracking-[0.67px]",
     "text-2xl leading-9",
-    "text-xl leadning-[30px]",
+    "text-xl leading-[30px]",
     "text-base leading-6",
     "text-[13px] leading-[19px]",
   ];
 
   return (
-    <span className={cn("tracking-normal", presetList[preset - 1])}>
+    <El className={cn("tracking-normal", presetList[preset - 1], className)}>
       {children}
-    </span>
+    </El>
   );
 };
