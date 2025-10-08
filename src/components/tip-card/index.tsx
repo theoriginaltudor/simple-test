19b0fba9238ccd794c9d399181864ee5bc8b55d7
Tudor Caseru <theoriginaltudor@gmail.com>
2025-10-08 14:26:40 +0200

    conditional disabled

--- src/components/tip-card/index.tsx
+++ src/components/tip-card/index.tsx
@@ -1,14 +1,24 @@
+import type { FC } from "react";
 import { TipButton } from "../tip-button";
 import { Amount } from "./label-amount";
 
-export const Card = () => {
+export const Card: FC<{ tip: number; total: number; onReset: () => void }> = ({
+  onReset,
+  tip,
+  total,
+}) => {
+  const disabled = tip + total == 0;
   return (
     <div className="bg-green-900 py-6 px-8 rounded-[5px] flex flex-col gap-32">
       <div className="flex-col gap-6">
-        <Amount label="tip Amount">$4.27</Amount>
-        <Amount label="Total">$32.79</Amount>
+        <Amount label="tip Amount">${tip}</Amount>
+        <Amount label="Total">${total}</Amount>
       </div>
-      <TipButton disabled className="uppercase w-full">
+      <TipButton
+        disabled={disabled}
+        className="uppercase w-full"
+        onClick={() => onReset()}
+      >
         Reset
       </TipButton>
     </div>
