ba98b4f6fcc07a29924559082dbdf7210b93a64a
Tudor Caseru <theoriginaltudor@gmail.com>
2025-10-08 13:48:06 +0200

    basic view default

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
 import Dollar from "./assets/images/icon-dollar.svg?react";
 import Person from "./assets/images/icon-person.svg?react";
+import Logo from "./assets/images/logo.svg?react";
 import { Field } from "./components/form-field";
 import { Input } from "./components/form-field/form-input";
 import { Label } from "./components/form-field/form-label";
@@ -9,41 +9,36 @@ import { TipButton } from "./components/tip-button";
 import { Card } from "./components/tip-card";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <Text preset={1} as={"h2"} className="text-grey-400">
-        Vite + React
-      </Text>
-
-      <div className="rounded-2xl m-1 p-3">
-        <TipButton
-          preset="light"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          {count}%
-        </TipButton>
-        <p className="text-green-400 text-sm leading-4 tracking-normal">
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+    <div className="flex flex-col items-center pt-40 bg-grey-200 min-h-dvh gap-[88px]">
+      <Logo />
+      <div className="bg-white rounded-[25px] p-8 flex gap-12">
+        <div className="flex gap-10 flex-col">
+          <Field>
+            <Label>Bill</Label>
+            <Input icon={Dollar} strategy={parseFloat} />
+          </Field>
+          <div className="flex flex-col gap-2">
+            <Text as="span" preset={5}>
+              Select Tip %
+            </Text>
+            <div className="grid grid-cols-3 gap-4">
+              <TipButton preset="dark">5%</TipButton>
+              <TipButton preset="dark">10%</TipButton>
+              <TipButton preset="dark">15%</TipButton>
+              <TipButton preset="dark">25%</TipButton>
+              <TipButton preset="dark">50%</TipButton>
+              <TipButton preset="light">Custom</TipButton>
+            </div>
+          </div>
+          <Field>
+            <Label>Number of People</Label>
+            <Input icon={Person} />
+          </Field>
+        </div>
+        <Card />
       </div>
-      <p className="text-green-200">
-        <Text>Click on the Vite and React logos to learn more</Text>
-      </p>
-
-      <Field>
-        <Label className="text-grey-500">Number of People</Label>
-        <Input icon={Person}></Input>
-      </Field>
-
-      <Field>
-        <Label className="text-grey-500">Money</Label>
-        <Input icon={Dollar} strategy={parseFloat}></Input>
-      </Field>
-
-      <Card />
-    </>
+    </div>
   );
 }
 
