8d61d2c6552baf235ffd03be6a9559824f0aea78
Tudor Caseru <theoriginaltudor@gmail.com>
2025-10-08 14:34:35 +0200

    fix decimals

--- src/components/tip-card/index.tsx
+++ src/components/tip-card/index.tsx
@@ -11,8 +11,8 @@ export const Card: FC<{ tip: number; total: number; onReset: () => void }> = ({
   return (
     <div className="bg-green-900 py-6 px-8 rounded-[5px] flex flex-col gap-32">
       <div className="flex-col gap-6">
-        <Amount label="tip Amount">${tip}</Amount>
-        <Amount label="Total">${total}</Amount>
+        <Amount label="tip Amount">${tip.toFixed(2)}</Amount>
+        <Amount label="Total">${total.toFixed(2)}</Amount>
       </div>
       <TipButton
         disabled={disabled}
