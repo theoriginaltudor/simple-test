abd4d38a5fdf0ce20d6a5ea84401f67ef0382d6e
Tudor Caseru <theoriginaltudor@gmail.com>
2025-10-08 10:11:56 +0200

    new svg plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import svgr from "vite-plugin-svgr";
 
-export const pluginList = [react(), tailwindcss()];
+export const pluginList = [react(), tailwindcss(), svgr()];
 
 // https://vite.dev/config/
 export default defineConfig({
