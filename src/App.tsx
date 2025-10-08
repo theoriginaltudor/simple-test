1b28bea884ac07bdec8bcb250ca3cf7c4237d652
Tudor Caseru <theoriginaltudor@gmail.com>
2025-10-08 14:50:11 +0200

    fix 0 and 0 problem plus add deselect functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Dollar from "./assets/images/icon-dollar.svg?react";
 import Person from "./assets/images/icon-person.svg?react";
 import Logo from "./assets/images/logo.svg?react";
@@ -9,12 +10,21 @@ import { TipButton } from "./components/tip-button";
 import { Card } from "./components/tip-card";
 
 function App() {
+  const [bill, setBill] = useState<number | null>(null);
+  const [people, setPeople] = useState<number | null>(null);
+  const [tip, setTip] = useState<number | null>(null);
+  let tipForPerson;
+  let totalForPerson;
+  if (bill && people && tip && bill > 0 && people > 0) {
+    tipForPerson = (bill * tip) / 100 / people;
+    totalForPerson = bill / people + tipForPerson;
+  }
   return (
     <div className="flex flex-col items-center pt-40 bg-grey-200 min-h-dvh gap-[88px]">
       <Logo />
       <div className="bg-white rounded-[25px] p-8 flex gap-12">
         <div className="flex gap-10 flex-col">
-          <Field>
+          <Field value={bill} setValue={setBill}>
             <Label>Bill</Label>
             <Input icon={Dollar} strategy={parseFloat} />
           </Field>
@@ -23,20 +33,68 @@ function App() {
               Select Tip %
             </Text>
             <div className="grid grid-cols-3 gap-4">
-              <TipButton preset="dark">5%</TipButton>
-              <TipButton preset="dark">10%</TipButton>
-              <TipButton preset="dark">15%</TipButton>
-              <TipButton preset="dark">25%</TipButton>
-              <TipButton preset="dark">50%</TipButton>
-              <TipButton preset="light">Custom</TipButton>
+              <TipButton
+                onClick={() => {
+                  if (tip == 5) setTip(null);
+                  else setTip(5);
+                }}
+                preset={tip == 5 ? "base" : "dark"}
+              >
+                5%
+              </TipButton>
+              <TipButton
+                onClick={() => {
+                  if (tip == 10) setTip(null);
+                  else setTip(10);
+                }}
+                preset={tip == 10 ? "base" : "dark"}
+              >
+                10%
+              </TipButton>
+              <TipButton
+                onClick={() => {
+                  if (tip == 15) setTip(null);
+                  else setTip(15);
+                }}
+                preset={tip == 15 ? "base" : "dark"}
+              >
+                15%
+              </TipButton>
+              <TipButton
+                onClick={() => {
+                  if (tip == 25) setTip(null);
+                  else setTip(25);
+                }}
+                preset={tip == 25 ? "base" : "dark"}
+              >
+                25%
+              </TipButton>
+              <TipButton
+                onClick={() => {
+                  if (tip == 50) setTip(null);
+                  else setTip(50);
+                }}
+                preset={tip == 50 ? "base" : "dark"}
+              >
+                50%
+              </TipButton>
+              <TipButton>Custom</TipButton>
             </div>
           </div>
-          <Field>
+          <Field value={people} setValue={setPeople}>
             <Label>Number of People</Label>
             <Input icon={Person} />
           </Field>
         </div>
-        <Card />
+        <Card
+          tip={tipForPerson && tipForPerson >= 0 ? tipForPerson : 0}
+          total={totalForPerson && totalForPerson >= 0 ? totalForPerson : 0}
+          onReset={() => {
+            setBill(null);
+            setPeople(null);
+            setTip(null);
+          }}
+        />
       </div>
     </div>
   );
